b357e217e2213062e6eed21282193753f06fec2e
cole-hartman <colehartman00@gmail.com>
2025-03-22 13:17:43 -0700

    u

---
+++ lua/akira/plugins/transparent.lua
@@ -0,0 +1,41 @@
+return{  
+  {
+    'xiyaowong/transparent.nvim',
+    config = function()
+      require('transparent').setup {
+        groups = { -- default groups
+          'Normal',
+          'NormalNC',
+          'Comment',
+          'Constant',
+          'Special',
+          'Identifier',
+          'Statement',
+          'PreProc',
+          'Type',
+          'Underlined',
+          'Todo',
+          'String',
+          'Function',
+          'Conditional',
+          'Repeat',
+          'Operator',
+          'Structure',
+          'LineNr',
+          'NonText',
+          'SignColumn',
+          'CursorLine',
+          'CursorLineNr',
+          'StatusLine',
+          'StatusLineNC',
+          'EndOfBuffer',
+        },
+        extra_groups = {
+          'NormalFloat', -- plugins with float panels
+          'NvimTreeNormal', -- NvimTree
+        },
+        exclude_groups = {}, -- groups you don't want to clear
+      }
+    end,
+  },
+}
--- lua/akira/plugins/treesitter.lua
+++ lua/akira/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter",
+	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
 		require'nvim-treesitter.configs'.setup {
@@ -15,14 +15,6 @@ return {
 
   highlight = {
     enable = true,
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
-
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
@@ -33,3 +25,5 @@ return {
 	end,
 }
 }
+
+
